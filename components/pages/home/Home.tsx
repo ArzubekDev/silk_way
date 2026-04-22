ef80776e4d5a0e877bd62fd736224dbca7dfd912
ArzubekDev <arzubekmain@gmail.com>
2026-04-22 14:46:31 +0600

    style(Card)

--- components/pages/home/Home.tsx
+++ components/pages/home/Home.tsx
@@ -1,10 +1,9 @@
 'use client'
 import Card from '@/ui/card/Card';
-import './Home.scss';
 import { useQuery } from '@tanstack/react-query';
 import { PRODUCT_KEYS } from '@/shared/api/query-keys';
 import { getProducts } from '@/shared/api/product.api';
-
+import style from './style.module.scss'
 
 const Home = () => {
     const { data: products } = useQuery<any>({
@@ -12,10 +11,11 @@ const Home = () => {
     queryFn: getProducts,
     staleTime: 1000 * 60 * 5,
   });
+  console.log(products, "PRODUCTSSSS");
   
   return (
-    <section className="home">
-      <div className="home--content">
+    <section className={style.home}>
+      <div className={style.content}>
         {products?.map((el:any) => <Card el={el} key={el.id} />)}
       </div>
     </section>
